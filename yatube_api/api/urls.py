0ddbcc8467b94b81dafce2c8ec5da8ddf80bd568
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-06-28 18:45:17 +0300

    Upload

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
+
+v1_router = DefaultRouter()
+v1_router.register(r'posts', PostViewSet, basename='posts')
+v1_router.register(r'groups', GroupViewSet, basename='groups')
+v1_router.register(r'follow', FollowViewSet, basename='follow')
+v1_router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
+                   basename='comments')
+
 
 urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,71 @@
-# TODO:  Напишите свой вариант
+from rest_framework import filters, permissions, viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.pagination import LimitOffsetPagination
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+
+from posts.models import Post, Comment, Group, Follow
+from api.serializers import (PostSerializer, GroupSerializer,
+                             CommentSerializer, FollowSerializer)
+from .permissions import IsAuthorOrReadOnlyPermission
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [permissions.IsAuthenticated,
+                                       IsAuthorOrReadOnlyPermission]
+        return super().get_permissions()
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_post(self):
+        post_id = self.kwargs['post_id']
+        return get_object_or_404(Post, id=post_id)
+
+    def get_queryset(self):
+        post = self.get_post()
+        return Comment.objects.filter(post=post)
+
+    def perform_create(self, serializer):
+        post = self.get_post()
+        serializer.save(author=self.request.user, post=post)
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [permissions.IsAuthenticated,
+                                       IsAuthorOrReadOnlyPermission]
+        return super().get_permissions()
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (filters.SearchFilter)
+    search_fields = ('following',)
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        following_user = serializer.validated_data['following']
+        if self.request.user == following_user:
+            raise ValidationError("Вы не можете отслеживать себя")
+        serializer.save(user=self.request.user)
