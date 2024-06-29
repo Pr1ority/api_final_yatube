2224a01c7e885ad2f4773e7a7fcd0548d9ad968d
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-06-30 02:40:03 +0300

    Update

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,9 +2,9 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
-User = get_user_model()
-
 from posts.models import Comment, Post, Group, Follow
+
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -35,12 +35,13 @@ class GroupSerializer(serializers.ModelSerializer):
 
 class FollowSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(slug_field='username', read_only=True)
-    following = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
+    following = serializers.SlugRelatedField(slug_field='username',
+                                             queryset=User.objects.all())
 
     class Meta:
         model = Follow
         fields = ('id', 'user', 'following')
-        
+
     def validate_following(self, value):
         if self.context['request'].user == value:
             raise serializers.ValidationError("Вы не можете отслеживать себя")
