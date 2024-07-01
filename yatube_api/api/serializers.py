5258ed8493d5b394bfa994d2e0a3d1a206b2d19b
Aleksey Bondarenko <alioshenkaia@yandex.ru>
2024-07-01 20:31:04 +0300

    Update

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -8,7 +8,7 @@ User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = SlugRelatedField(slug_field='username')
 
     class Meta:
         fields = '__all__'
